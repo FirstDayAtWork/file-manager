f684eb24c0667df8754435d4c3d4e4f0c1e90faf
Michael <fatlycka@gmail.com>
2025-11-01 04:20:27 +0300

    feat: add ls && cd commands

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url';
 import readline from 'node:readline/promises';
 import os from 'node:os';
 import { parseArgs } from './utils/parseArgs.js';
+import { checkType } from './utils/checkType.js';
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -21,7 +22,7 @@ const __dirname = path.dirname(__filename);
   console.log(`Welcome to the File Manager, \x1b[34m${args.username ?? ''}\x1b[0m!`);
   console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
 
-  rl.on('line', (answer) => {
+  rl.on('line', async (answer) => {
     if (answer.trim() === '.exit') {
         console.log(`\nThank you for using File Manager, \x1b[34m${args.username}\x1b[0m!, goodbye!`);
         rl.close();
@@ -29,8 +30,40 @@ const __dirname = path.dirname(__filename);
 
     if (answer.trim() === 'up') {
       currentPath = (/^\w:\\{1}(?!\s+|\S+)/.test(currentPath) || /^\/(?!\s+|\S+)/.test(currentPath)) ? currentPath : path.dirname(currentPath);
-      
+
       console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
+    }
+
+    if (answer.trim() === 'ls') {
+       try {
+          const currentDir = await fs.readdir(currentPath, { withFileTypes: true });
+          const array = currentDir.reduce((a, b) => {
+            const object = {
+              name: b.name.slice(0, 20),
+              type: checkType(b)
+            }
+            return [...a, object];
+          }, []);
+
+          console.table(array);
+       } catch (error) {
+          console.log(`\x1b[31mOperation failed\x1b[0m!, can't access \x1b[31m${currentPath}\x1b[0m`)
+       }
+      console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
+    }
+
+    if (/^cd\s+\w+/.test(answer.trim())) {
+       const value = answer.trim().replace(/^cd\s+/, '');
+       const testPath = path.join(currentPath, value);
+
+       try {
+         await fs.access(testPath);
+         currentPath = testPath;
+       } catch (error) {
+         console.log(`\x1b[31mOperation failed\x1b[0m!, can't access \x1b[31m${testPath}\x1b[0m`)
+       }
+       
+       console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
     }
   })
   
