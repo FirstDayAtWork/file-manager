fc19587323e7fbc1d5e3996e83a29d4a6fa233cf
Michael <fatlycka@gmail.com>
2025-11-01 18:23:24 +0300

    feat: add cat command && fix some bugs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import readline from 'node:readline/promises';
+import { pipeline } from 'stream/promises';
 import os from 'node:os';
 import { parseArgs } from './utils/parseArgs.js';
 import { checkType } from './utils/checkType.js';
@@ -14,27 +15,28 @@ const __dirname = path.dirname(__filename);
 ;(() => {
   let currentPath = os.homedir();
   const args = parseArgs();
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
   console.log(`Welcome to the File Manager, \x1b[34m${args.username ?? ''}\x1b[0m!`);
   console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
 
   rl.on('line', async (answer) => {
-    if (answer.trim() === '.exit') {
+    answer = answer.trim();
+    if (answer === '.exit') {
         console.log(`\nThank you for using File Manager, \x1b[34m${args.username}\x1b[0m!, goodbye!`);
         rl.close();
     }
 
-    if (answer.trim() === 'up') {
+    if (answer === 'up') {
       currentPath = (/^\w:\\{1}(?!\s+|\S+)/.test(currentPath) || /^\/(?!\s+|\S+)/.test(currentPath)) ? currentPath : path.dirname(currentPath);
 
       console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
     }
 
-    if (answer.trim() === 'ls') {
+    if (answer === 'ls') {
        try {
           const currentDir = await fs.readdir(currentPath, { withFileTypes: true });
           const array = currentDir.reduce((a, b) => {
@@ -52,8 +54,8 @@ const __dirname = path.dirname(__filename);
       console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
     }
 
-    if (/^cd\s+\w+/.test(answer.trim())) {
-       const value = answer.trim().replace(/^cd\s+/, '');
+    if (/^cd\s+\w+/.test(answer)) {
+       const value = answer.replace(/^cd\s+/, '');
        const testPath = path.join(currentPath, value);
 
        try {
@@ -61,6 +63,24 @@ const __dirname = path.dirname(__filename);
          currentPath = testPath;
        } catch (error) {
          console.log(`\x1b[31mOperation failed\x1b[0m!, can't access \x1b[31m${testPath}\x1b[0m`)
+       }
+       
+       console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
+    }
+
+    if (/^cat\s+\w+/.test(answer)) {
+       const value = answer.replace(/^cat\s+/, '');
+       const testPath = path.join(currentPath, value);
+
+       try {
+         const file = await fs.open(testPath);
+         const stream = file.createReadStream();
+
+         await pipeline(stream, process.stdout, { end: false })
+         console.log('\n' + 'Success')
+         
+       } catch (error) {
+         console.log(`\x1b[31mOperation failed\x1b[0m!, can't access \x1b[31m${testPath}\x1b[0m`, error)
        }
        
        console.log(`\nYou are currently in \x1b[32m${currentPath}\x1b[0m`);
--- utils/parseArgs.js
+++ utils/parseArgs.js
@@ -1,9 +1,18 @@
 import process from 'process'
 
+const regex = /(?<=--)\w+(?=\=)/;
+
 export const parseArgs = () => {
-  return process.argv.slice(2).reduce((a, b) => {
-    const key = (/(?<=--)\w+(?=\=.+)/).exec(b)[0];
-    a[key] = b.replace(/^--\w+=/, '');
-    return a;
+  const array = process.argv.slice(2);
+
+  if (array.length === 0) return { username: 'Anonymous' };
+
+  return array.reduce((a, b) => {
+    if (regex.test(b)) {
+      const key = (regex).exec(b)[0];
+      const value = b.replace(/^--\w+=/, '').trim();
+      a[key] = value || 'Anonymous';
+      return a;
+    }
   }, {})
 };
